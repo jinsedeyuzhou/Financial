147644f30955ba89b2f26e9c4992a3e5295aa7c0
ebrightmoon <jinsedeyuzhou@sina.com>
2017-11-09 12:04:34 +0800

    update

---
+++ financial/src/main/java/com/financial/android/activity/effects/RePastActivity.java
@@ -0,0 +1,38 @@
+package com.financial.android.activity.effects;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.financial.android.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/11/9.
+ */
+
+public class RePastActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    protected void bindEvent() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
